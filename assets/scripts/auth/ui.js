49937c52a3ed8bab77032e8153d43d70102e5c4b
“sstone72389” <sstone72389@gmail.com>
2017-04-11 14:35:41 -0400

    Adds alerts for UA features
    Adds temporary alerts to be removed once JS logic is complete

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -3,38 +3,46 @@ const store = require('../store.js')
 
 const signUpSuccess = (data) => {
   console.log(data)
+  alert('You have signed up')
 }
 
 const signUpFailure = (error) => {
   console.error(error)
+  alert('Unsuccessful sign up')
 }
 
 const signInSuccess = (data) => {
   console.log(data)
+  alert('You have signed in')
   // store the user object as per below
   store.user = data.user
 }
 
 const signInFailure = (error) => {
   console.error(error)
+  alert('Unsuccessful sign in')
 }
 
 const changePasswordSuccess = (data) => {
   console.log('password changed', data)
+  alert('Successful PW Change')
 }
 
 const changePasswordFailure = (error) => {
   console.error(error)
+  alert('Unsuccessful PW change')
 }
 
 const signOutSuccess = (data) => {
   console.log('You have signed out!')
+  alert('You have signed out')
   // store the user with a value of null as per below
   store.user = null
 }
 
 const signOutFailure = (error) => {
   console.error(error)
+  alert('Unsuccessful sign in')
 }
 
 module.exports = {
